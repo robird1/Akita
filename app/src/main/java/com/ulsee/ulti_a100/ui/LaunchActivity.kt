6654ec031c5280d4fdf5072c715fde41a5b377ed
steve.su <steve.su@ulsee.com>
2020-12-09 16:55:30 +0800

    migrate to Android 11 -  One-time permission

--- app/src/main/java/com/ulsee/ulti_a100/ui/LaunchActivity.kt
+++ app/src/main/java/com/ulsee/ulti_a100/ui/LaunchActivity.kt
@@ -1,146 +1,31 @@
 package com.ulsee.ulti_a100.ui
 
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
+import android.Manifest
 import android.os.Bundle
-import android.provider.Settings
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.ulsee.ulti_a100.MainActivity
 import com.ulsee.ulti_a100.R
-import com.ulsee.ulti_a100.model.AppPreference
-import com.ulsee.ulti_a100.model.PermissionAskPreference
-import com.ulsee.ulti_a100.model.RealmDevice
-import io.realm.Realm
-import io.realm.kotlin.where
+import com.ulsee.ulti_a100.utils.PermissionController
 import java.util.*
 
+
+private val TAG = LaunchActivity::class.java.simpleName
+
 class LaunchActivity : AppCompatActivity() {
-
-    val NEEDED_PERMISSIONS = arrayOf<String>(
-        android.Manifest.permission.READ_EXTERNAL_STORAGE,
-        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//        android.Manifest.permission.ACCESS_WIFI_STATE,
-        android.Manifest.permission.INTERNET,
-        android.Manifest.permission.CAMERA,
-//        android.Manifest.permission.ACCESS_FINE_LOCATION,
-//        android.Manifest.permission.ACCESS_COARSE_LOCATION
-    )
-
-    val REQUEST_CODE_PERMISSION = 11111;
-    val REQUEST_CODE_GO_PERMISSION_SETTINGS = 11112;
-
-    lateinit var permissionAskPreference : PermissionAskPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
 
-        permissionAskPreference = PermissionAskPreference(getSharedPreferences("permission", Context.MODE_PRIVATE))
-
         // permission
-        Timer().schedule(object: TimerTask() {
+        Timer().schedule(object : TimerTask() {
             override fun run() {
-                runOnUiThread { validatePermissions() }
-            }
-        }, 200)
-    }
-
-    private fun validatePermissions() {
-        if (isPermissionsAllGranted()) {
-            goNextPage()
-        } else if (isUserRequestNeverAskPermissionAgain()) {
-            Toast.makeText(
-                this,
-                "Miss permission, but You require never ask permission again",
-                Toast.LENGTH_SHORT
-            ).show()
-            goPermissionSettings()
-        } else {
-            requestPermission()
-        }
-    }
-
-    // TODO
-    private fun goNextPage() {
-        val appPreference = AppPreference(getSharedPreferences("app", MODE_PRIVATE))
-        val hasDevice = Realm.getDefaultInstance().where<RealmDevice>().findAll().size > 0
-//        if (appPreference.isOnceCreateFirstDevice() || hasDevice ) {
-//            startActivity(Intent(this, MainActivity::class.java))
-//        } else {
-//            startActivity(Intent(this, StarterActivity::class.java))
-//        }
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
-    }
-
-    private fun goPermissionSettings() {
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-        val uri = Uri.fromParts("package", packageName, null)
-        intent.data = uri
-        startActivityForResult(
-            intent,
-            REQUEST_CODE_GO_PERMISSION_SETTINGS
-        )
-    }
-
-    private fun getNotAllowedPermissions(): List<String> {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return ArrayList()
-        }
-        val notAllowedPermisions: MutableList<String> =
-            ArrayList()
-        for (permission in NEEDED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                notAllowedPermisions.add(permission)
-            }
-        }
-        return notAllowedPermisions
-    }
-
-    private fun isPermissionsAllGranted(): Boolean {
-        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            true
-        } else getNotAllowedPermissions().size == 0
-    }
-
-    private fun isUserRequestNeverAskPermissionAgain(): Boolean {
-        var isAnyonePermissionAskable = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            isAnyonePermissionAskable = false
-            val notAllowedPermisions =
-                getNotAllowedPermissions()
-            for (permission in notAllowedPermisions) {
-                val isPermissionEverAsked: Boolean =
-                    permissionAskPreference.isPermissionEverAsked(permission)
-                isAnyonePermissionAskable =
-                    isAnyonePermissionAskable || !(shouldShowRequestPermissionRationale(permission!!) == false && isPermissionEverAsked)
-                if (!isPermissionEverAsked) {
-                    permissionAskPreference.setPermissionAsked(permission)
+                runOnUiThread {
+                    PermissionController().requestPermission(this@LaunchActivity, Manifest.permission.CAMERA)
                 }
             }
-        }
-        return !isAnyonePermissionAskable
-    }
-
-    private fun requestPermission(): Boolean {
-        val notAllowedPermisions =
-            getNotAllowedPermissions()
-        ActivityCompat.requestPermissions(
-            this,
-            notAllowedPermisions.toTypedArray(),
-            REQUEST_CODE_PERMISSION
-        )
-        return false
+        }, 200)
     }
 
     override fun onRequestPermissionsResult(
@@ -148,11 +33,14 @@ class LaunchActivity : AppCompatActivity() {
         permissions: Array<String?>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            REQUEST_CODE_PERMISSION -> {
-                validatePermissions()
-            }
-        }
+        Log.d(TAG, "[Enter] onRequestPermissionsResult")
+
+//        when (requestCode) {
+//            REQUEST_CODE_PERMISSION -> {
+//                validatePermissions()
+//            }
+//        }
+        PermissionController().onRequestPermissionsResult(requestCode, permissions, grantResults, this, MainActivity::class.java)
     }
 
 }
